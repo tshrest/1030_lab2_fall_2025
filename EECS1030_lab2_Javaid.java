8f608404f5d1226218172f3ac81a3a206bb2283b
tshrest <tshrest@rockets.utoledo.edu>
2025-09-10 23:43:40 -0400

    java file

---
+++ EECS1030_lab2_Javaid.java
@@ -0,0 +1,11 @@
+/* Created by Ahmad Javaid on 2025-09-05 */
+
+package EECS1030_lab2_Javaid;
+
+public class EECS1030_lab2_Javaid {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
+
+// This is a simple Java application that prints "Hello, World!" to the console.
